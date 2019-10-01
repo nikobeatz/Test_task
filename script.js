5e8b9553273ea8636822b7461dc31b734c0e9b1f
nikobeatz <nikobeatz91205@gmail.com>
2019-10-01 19:50:49 +0300

    end add responsivity

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 'use strict';
 
 
+// show menu
+
+let menu = document.querySelector('.menu');
+let menuOpenBtn = document.querySelector('.bottom-wrapper__menu-button button');
+menu.style.display = 'none'
+
+menuOpenBtn.addEventListener("click", function () {
+
+  if (menu.style.display == 'none') menu.style.display = 'block';
+  else menu.style.display = 'none'
+
+});
+
+//end show menu
+
+
 // start services slider 
 
 $(document).ready(function(){
@@ -9,6 +25,30 @@ $(document).ready(function(){
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            }
+          },
+          {
+            breakpoint: 520,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
   });
 
